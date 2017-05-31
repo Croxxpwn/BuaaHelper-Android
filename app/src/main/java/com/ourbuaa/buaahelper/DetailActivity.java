3987b7171c452948602f9026d91e98ac917ad46a
Croxx <croxxpwn@126.com>
2017-05-31 16:22:44 +0800

    Add Download (View(uncompleted)+function);

--- app/src/main/java/com/ourbuaa/buaahelper/DetailActivity.java
+++ app/src/main/java/com/ourbuaa/buaahelper/DetailActivity.java
@@ -1,14 +1,25 @@
 package com.ourbuaa.buaahelper;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -16,6 +27,7 @@ public class DetailActivity extends AppCompatActivity {
     private TextView departmentTW;
     private TextView timeTW;
     private TextView contentTW;
+
 
     int id;
 
@@ -25,8 +37,42 @@ public class DetailActivity extends AppCompatActivity {
             int id = params[0];
             ClientUtils.ReadNotification(SharedData.getU(), id);
             DBNotificationDao dao = new DBNotificationDao(DetailActivity.this);
-            dao.ReadNotification(id,SharedData.getU().getUsername());
+            dao.ReadNotification(id, SharedData.getU().getUsername());
             return null;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
+
+    class DownloadFileTask extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            String url = params[0];
+            String name = params[1];
+
+            String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/BuaaHelper/";
+            try {
+                HttpsUtils.downLoadFromUrl(url, name, path);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean state) {
+            if (state) {
+                Toast.makeText(DetailActivity.this, "下载完成！目录：" + Environment.getExternalStorageDirectory().getAbsolutePath()+"/Download/BuaaHelper", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(DetailActivity.this, "下载失败！", Toast.LENGTH_SHORT).show();
+            }
+            super.onPostExecute(state);
         }
     }
 
@@ -80,13 +126,50 @@ public class DetailActivity extends AppCompatActivity {
             public void onClick(View v) {
                 new ReadNotificationTask().execute(id);
                 button_read.setVisibility(View.INVISIBLE);
-                Toast.makeText(DetailActivity.this,"阅读成功",Toast.LENGTH_SHORT).show();
+                Toast.makeText(DetailActivity.this, "阅读成功", Toast.LENGTH_SHORT).show();
             }
         });
 
         if (bean.getImportant() == 0 || bean.getRead() == 1) {
             button_read.setVisibility(View.INVISIBLE);
         }
+
+        LinearLayout downlist_layout = (LinearLayout) findViewById(R.id.downlist_layout);
+
+        try {
+            JSONArray fileJSONArray = new JSONArray(bean.getFiles());
+            for (int i = 0; i < fileJSONArray.length(); i++) {
+                JSONObject mJSONObject = fileJSONArray.getJSONObject(i);
+                final String name = mJSONObject.getString("fileName");
+                final String url = mJSONObject.getString("url");
+                LinearLayout itemView = (LinearLayout) getLayoutInflater().inflate(R.layout.download_filelist_item, downlist_layout, false);
+                TextView nameView = (TextView) itemView.findViewById(R.id.downlist_item_name);
+                nameView.setText(name);
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (ContextCompat.checkSelfPermission(DetailActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                            ActivityCompat.requestPermissions(DetailActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                        } else {
+                            new DownloadFileTask().execute(url, name);
+                            Toast.makeText(DetailActivity.this, "正在开始下载...", Toast.LENGTH_SHORT).show();
+
+                        }
+                        //ActivityCompat.requestPermissions(DetailActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                        //new DownloadFileTask().execute("https://www.ourbuaa.com/file/download/5ecdefde0ed0432c27be8519a9a3ef954662f96a", "file");
+                    }
+                });
+                downlist_layout.addView(itemView);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        //ActivityCompat.requestPermissions(DetailActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        //https://www.ourbuaa.com/file/download/5ecdefde0ed0432c27be8519a9a3ef954662f96a
 
     }
 }
